bd573b5fed96e00493b62cb810f7cac85b138903
Abdullah <abdullahaha101@gmail.com>
2024-02-02 16:58:37 +0500

    updated slider component

--- next-task/src/pages/components/Slider.jsx
+++ next-task/src/pages/components/Slider.jsx
@@ -1,70 +1,101 @@
-'use client';
-import React, { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from 'react-icons/fa';
-import { data } from '../data';
 import { FaAngleRight } from 'react-icons/fa';
-function Slider() {
-  let wrapper;
-  if (typeof window !== 'undefined') {
-    wrapper = document.querySelector('.card-wrapper');
-  }
+import { data } from '../data';
 
-  const handlePrevClick = () => {
-    let width = wrapper.clientWidth;
-    let totalWidth = wrapper.scrollWidth;
-    wrapper.scrollLeft = wrapper.scrollLeft - width;
-    if (wrapper.scrollLeft == 0) {
-      wrapper.scrollLeft = totalWidth;
+const Slider = () => {
+  const maxScrollWidth = useRef(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carousel = useRef(null);
+
+  const movePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevState) => prevState - 1);
     }
   };
 
-  const handleNextClick = () => {
-    let width = wrapper.clientWidth;
-    let totalWidth = wrapper.scrollWidth;
-    wrapper.scrollLeft = wrapper.scrollLeft + width;
-    if (wrapper.scrollLeft + width >= totalWidth) {
-      wrapper.scrollLeft = 0;
+  const moveNext = () => {
+    if (
+      carousel.current !== null &&
+      carousel.current.offsetWidth * currentIndex <= maxScrollWidth.current
+    ) {
+      setCurrentIndex((prevState) => prevState + 1);
     }
   };
+
+  const isDisabled = (direction) => {
+    if (direction === 'prev') {
+      return currentIndex <= 0;
+    }
+
+    if (direction === 'next' && carousel.current !== null) {
+      return (
+        carousel.current.offsetWidth * currentIndex >= maxScrollWidth.current
+      );
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    if (carousel !== null && carousel.current !== null) {
+      carousel.current.scrollLeft = carousel.current.offsetWidth * currentIndex;
+    }
+  }, [currentIndex]);
+
+  useEffect(() => {
+    maxScrollWidth.current = carousel.current
+      ? carousel.current.scrollWidth - carousel.current.offsetWidth
+      : 0;
+  }, []);
+
   return (
-    <>
-      <div className="lg:px-20 md:px-20 px-12 pt-12">
-        <div className=" flex justify-between">
+    <div className="carousel my-12 px-[28px]">
+      <div className="relative overflow-hidden">
+        <div className="px-1 lg:px-[40px] mb-6 flex justify-between">
           <h1 className="font-bold text-[30px]">Convert to PDF</h1>
           <button className="bg-gray-200/80 px-6 text-[14px] hover:bg-gray-200 flex justify-center items-center gap-1 rounded-full p-1">
             ALL
             <FaAngleRight />
           </button>
         </div>
-        <div className="relative lg:pr-9 pr-6">
+        <div className="w-full h-full">
           <button
-            className={`inline-block absolute top-[40%] left-[-40px] bg-white rounded-full p-3 border-[3px] border-gray-500`}>
-            <FaLongArrowAltLeft onClick={handlePrevClick} size={20} />
+            onClick={movePrev}
+            className=" text-black border-2 bg-white z-10 border-[black] p-4 rounded-full top-[30%] left-[0px] absolute opacity-100 disabled:opacity-25 disabled:cursor-not-allowed"
+            disabled={isDisabled('prev')}>
+            <FaLongArrowAltLeft className="z-0" />
           </button>
           <button
-            disabled={false}
-            onClick={handleNextClick}
-            className="inline-block right-arrow absolute top-[40%] right-[-40px] cursor-pointer bg-white rounded-full p-3 border-[3px] border-gray-500">
-            <FaLongArrowAltRight color="grey" size={20} />
+            onClick={moveNext}
+            className="p-4 absolute top-[30%] right-0 border-[black] border-2 rounded-full  hover:opacity-100 disabled:opacity-25 disabled:cursor-not-allowed z-10"
+            disabled={isDisabled('next')}>
+            <FaLongArrowAltRight />
           </button>
-          <div
-            className={`flex card-wrapper lg:gap-[24px] gap-[16px] overflow-x-hidden scroll-smooth py-12`}>
-            {data.map((d) => {
-              return (
-                <div
-                  key={d.id}
-                  className={`p-[24px] lg:w-[20%] md:w-[46.99%] w-[99%] shrink-0 border cursor-pointer duration-500 hover:shadow-xl rounded-md bg-white
-                      `}>
-                  <img className="w-[65px]" src={d.link} alt={d.alt} />
+        </div>
+        <div
+          ref={carousel}
+          className="carousel-container relative flex mx-12 gap-[32px] overflow-x-hidden pb-24 scroll-smooth snap-x snap-mandatory">
+          {data.map((d) => {
+            return (
+              <div
+                key={d.id}
+                className="carousel-item cursor-pointer duration-500 hover:shadow-xl border rounded-md p-[24px] relative w-64 h-64 snap-start">
+                <a className="h-full w-[100%] aspect-square block bg-no-repeat z-0">
+                  <img
+                    src={d.link}
+                    alt={d.alt}
+                    className="w-[65px] aspect-square"
+                  />
                   <h1 className="text-[20px] mt-[16px] font-bold">{d.alt}</h1>
                   <p className="text-[14px] mt-[12px] text-[grey]">{d.desc}</p>
-                </div>
-              );
-            })}
-          </div>
+                </a>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 export default Slider;
--- next-task/src/pages/index.js
+++ next-task/src/pages/index.js
@@ -2,13 +2,13 @@ import BookMarks from './components/BookMarks';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 import Slider from './components/Slider';
-
 export default function Home() {
   return (
     <div>
       <SearchBar />
       <Navbar />
       <BookMarks />
+      {/* <Slider /> */}
       <Slider />
     </div>
   );
