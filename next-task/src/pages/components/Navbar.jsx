ccd02beaec3f6b231db53e75792ff588692708f0
Abdullah <abdullahaha101@gmail.com>
2024-02-02 19:29:05 +0500

    modification to Navbar

--- next-task/src/pages/components/Navbar.jsx
+++ next-task/src/pages/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
   ];
   return (
     <div>
-      <div className="flex flex-wrap lg:flex-nowrap lg:text-[16px] text-[12px] lg:px-8 px-4  lg:py-3 py-2 gap-[16px] shadow-md lg:justify-between">
+      <div className="flex flex-wrap lg:flex-nowrap lg:text-[16px] text-[12px] lg:px-8 px-4  lg:py-3 py-2 gap-x-6 gap-y-2 shadow-md lg:justify-between">
         {navItems.map((items, index) => {
           return (
             <ul key={index}>
